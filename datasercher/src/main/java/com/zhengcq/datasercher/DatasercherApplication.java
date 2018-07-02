a6685a2d089a13665b9df4ce8166440f02da46ea
‘chuqiang.zheng <chuqiang.zheng@yoorstore.com>
2018-07-02 17:09:54 +0800

    init

---
+++ datasercher/src/main/java/com/zhengcq/datasercher/DatasercherApplication.java
@@ -0,0 +1,12 @@
+package com.zhengcq.datasercher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasercherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasercherApplication.class, args);
+	}
+}
---
+++ datasercher/src/test/java/com/zhengcq/datasercher/DatasercherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhengcq.datasercher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatasercherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
