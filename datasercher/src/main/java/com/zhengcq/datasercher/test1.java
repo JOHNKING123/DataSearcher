1e57535ee5b82ac8c9971cf421cc8deff211bfcf
‘chuqiang.zheng <chuqiang.zheng@yoorstore.com>
2018-07-12 09:55:35 +0800

    change test1 file

--- datasercher/src/main/java/com/zhengcq/datasercher/test1.java
+++ datasercher/src/main/java/com/zhengcq/datasercher/test1.java
@@ -3,5 +3,9 @@ package com.zhengcq.datasercher;
 public class test1 {
     public static  void main(String[] args){
         System.out.println("test");
+
+        System.out.println("asdf");
+        System.out.println("asd");
+        System.out.println("asdf");
     }
 }
