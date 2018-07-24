327baa98128377cbdf348059a885892e671e015a
‘chuqiang.zheng <chuqiang.zheng@yoorstore.com>
2018-07-24 11:59:39 +0800

    fix code

--- datasercher/src/main/java/com/zhengcq/datasercher/controller/TestController.java
+++ datasercher/src/main/java/com/zhengcq/datasercher/controller/TestController.java
@@ -3,6 +3,7 @@ package com.zhengcq.datasercher.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @PostMapping(value = "/test1")
-    public Object test(){
-        return "hello world";
+    public Object test(@RequestParam("name")String name){
+        return "hello world,"+name;
     }
 }
