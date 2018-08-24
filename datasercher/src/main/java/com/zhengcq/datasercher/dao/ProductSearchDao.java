c2e4e775595814a60eca70599bc9560c8ad46511
‘chuqiang.zheng <chuqiang.zheng@yoorstore.com>
2018-08-24 10:30:13 +0800

    Revert "fix code"
    This reverts commit a2aab0f

--- datasercher/src/main/java/com/zhengcq/datasercher/dao/ProductSearchDao.java
+++
@@ -1,7 +0,0 @@
-package com.zhengcq.datasercher.dao;
-
-import com.zhengcq.datasercher.entity.Product;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductSearchDao extends ElasticsearchRepository<Product,Long> {
-}
